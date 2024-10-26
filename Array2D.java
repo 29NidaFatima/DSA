2f838780c63528ab7f9f6471bd613e1a7635c5a6
NidaFatima <2529nida17@gmail.com>
2024-10-26 18:54:27 +0530

    .

--- Array2D.java
+++ Array2D.java
@@ -1,50 +1,52 @@
+
 // import java.util.*;
 import java.util.Scanner;
-public class Array2D {
-    public static boolean search(int matrix[][],int key) {
-    for (int i = 0; i < matrix.length; i++) {
-    for (int j = 0; j < matrix[0].length; j++) {
-      
-    if (  matrix[i][j]==key ) {
-    System.out.println("FOUND AT CELL (" + i + " ," + j+ ")" );
-    return true;
-    }
-    }
-    }
-    System.out.println("key not found");
-    return false;
-    }
 
-    //  finding largest no.from the matrix
-    public static boolean searchLargest(int matrix[][]) {
-        int largest = Integer.MIN_VALUE;  
-    
+public class Array2D {
+    public static boolean search(int matrix[][], int key) {
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 0; j < matrix[0].length; j++) {
-                if (matrix[i][j] > largest) {  
-                    largest = matrix[i][j]; 
+
+                if (matrix[i][j] == key) {
+                    System.out.println("FOUND AT CELL (" + i + " ," + j + ")");
+                    return true;
                 }
             }
         }
-    
-      
-        System.out.println("Largest value: " + largest);
-        return true;  
+        System.out.println("key not found");
+        return false;
     }
-    
-    // finding smallest no.from the matrix
-    public static boolean searchSmallest(int matrix[][]){
-int smallest=Integer.MAX_VALUE;
-for(int i=0; i<matrix.length;i++){
-    for(int j=0; j<matrix[0].length; j++){
-        if (matrix[i][j]<smallest) {
-            smallest=matrix[i][j];
+
+    // finding largest no.from the matrix
+    public static boolean searchLargest(int matrix[][]) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] > largest) {
+                    largest = matrix[i][j];
+                }
+            }
         }
+
+        System.out.println("Largest value: " + largest);
+        return true;
     }
-}
-System.out.println("Smallest value: " + smallest);
-return true;  
+
+    // finding smallest no.from the matrix
+    public static boolean searchSmallest(int matrix[][]) {
+        int smallest = Integer.MAX_VALUE;
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                if (matrix[i][j] < smallest) {
+                    smallest = matrix[i][j];
+                }
+            }
+        }
+        System.out.println("Smallest value: " + smallest);
+        return true;
     }
+
     // --------SPIRAL MATRIX---------
     public static void printSpiral(int matrix[][]) {
         int startRow = 0;
@@ -54,25 +56,25 @@ return true;
         while (startRow <= endRow && startColumn <= endColumn) {
             // TOP
             for (int j = startColumn; j <= endColumn; j++) {
-                System.out.print(matrix[startRow][j]+" ");
+                System.out.print(matrix[startRow][j] + " ");
             }
             // RIGHT
             for (int i = startRow + 1; i <= endRow; i++) {
-                System.out.print(matrix[i][endColumn]+" ");
+                System.out.print(matrix[i][endColumn] + " ");
             }
             // BOTTOM
             for (int j = endColumn - 1; j >= startColumn; j--) {
                 if (startRow == endRow) {
                     break;
                 }
-                System.out.print(matrix[endRow][j]+" ");
+                System.out.print(matrix[endRow][j] + " ");
             }
             // LEFT
             for (int i = endRow - 1; i >= startRow + 1; i--) {
                 if (startColumn == endColumn) {
                     break;
                 }
-                System.out.print(matrix[i][startColumn]+" ");
+                System.out.print(matrix[i][startColumn] + " ");
             }
             startRow++;
             startColumn++;
@@ -81,66 +83,67 @@ return true;
         }
         System.out.println();
     }
-    //---------------DIAGONAL SUM------------
+
+    // ---------------DIAGONAL SUM------------
     // (Brute Force Approach)
-public static int diagonalSum(int matrix[][]){
-    int sum=0;
-    for(int i=0; i<matrix.length;i++){
-        for(int j=0; j<matrix[0].length;j++){
-            //Primary Diagonal
-            if(i==j){
-                sum = sum + matrix[i][j];
+    public static int diagonalSum(int matrix[][]) {
+        int sum = 0; 
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[0].length; j++) {
+                // Primary Diagonal
+                if (i == j) {
+                    sum = sum + matrix[i][j];
+                }
+                // Secondary Diagonal
+                else if (i + j == matrix.length - 1) {
+                    sum = sum + matrix[i][j];
+                }
             }
-    //  Secondary  Diagonal
-           else if(i+j==matrix.length-1){
-            sum = sum + matrix[i][j];
-            }
-        } 
-     
+
+        }
+        return sum;
     }
-    return sum;
-}
-    //---------optimized way--------- 
+
+    // ---------optimized way---------
     public static int diagonalSum(int matrix[][]) {
         int sum = 0;
-        
+
         for (int i = 0; i < matrix.length; i++) {
             // Add primary diagonal element
             sum += matrix[i][i];
-            
-            // Add secondary diagonal element, but avoid double-counting the middle element in odd-sized matrices
+
+            // Add secondary diagonal element, but avoid double-counting the middle element
+            // in odd-sized matrices
             if (i != matrix.length - 1 - i) {
                 sum += matrix[i][matrix.length - 1 - i];
             }
         }
-        
+
         return sum;
     }
-    
- // // ----SEARCH IN SORTED MATRIX-----
- public static boolean stairCaseSearch(int matrix[][], int key) {
-    int row = 0;
-    int col = matrix[0].length - 1;
-    while (row < matrix.length && col >= 0) {
-        if (matrix[row][col] == key) {
-            System.out.println("Found Key at (" + row + "," + col + ")");
-            return true;
-        } else if (key < matrix[row][col]) {
-            col--;
-        } else {
-            row++;
+
+    //  --SEARCH IN SORTED MATRIX-----
+    public static boolean stairCaseSearch(int matrix[][], int key) {
+        int row = 0;
+        int col = matrix[0].length - 1;
+        while (row < matrix.length && col >= 0) {
+            if (matrix[row][col] == key) {
+                System.out.println("Found Key at (" + row + "," + col + ")");
+                return true;
+            } else if (key < matrix[row][col]) {
+                col--;
+            } else {
+                row++;
+            }
         }
+        System.out.println("Key not Found");
+        return false;
     }
-    System.out.println("Key not Found");
-    return false;
-}
-
-
 
     public static void main(String[] args) {
         int matrix[][] = new int[3][3];
         int n = matrix.length; // rows
-        int m = matrix[0].length; //columns
+        int m = matrix[0].length; // columns
 
         Scanner sc = new Scanner(System.in);
         // Taking input
@@ -157,21 +160,21 @@ public static int diagonalSum(int matrix[][]){
             }
             System.out.println();
         }
-        search(matrix,6);
-searchLargest(matrix);
-searchSmallest(matrix);
+        search(matrix, 6);
+        searchLargest(matrix);
+        searchSmallest(matrix);
         // --------SPIRAL MATRIX---------
         int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
         printSpiral(matrix);
 
-        //---------------DIAGONAL SUM------------
-         int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-        int sum= diagonalSum(matrix);
-        System.out.println("Sum of primary and secondary diagonal:"+ sum);
-       //----SEARCH IN SORTED MATRIX-----
+        // ---------------DIAGONAL SUM------------
+        int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
+        int sum = diagonalSum(matrix);
+        System.out.println("Sum of primary and secondary diagonal:" + sum);
+        // ----SEARCH IN SORTED MATRIX-----
 
         int matrix[][] = { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 }, { 13, 14, 15, 16 } };
-        int key=9;
+        int key = 9;
         stairCaseSearch(matrix, key);
     }
 }
--- Strings.java
+++ Strings.java
@@ -1,165 +1,194 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Strings {
-    // public static void printLetters(String str){
-    // for(int i=0; i<str.length();i++){
-    // System.out.print(str.charAt(i)+" ");
-    // }
-    
-    // System.out.println();
-    // }
-    // -------------Q1:CHECK IF A STRING IS A PALINDROME---------
-    // public static boolean isPalindrome(String str) {
-    //     for (int i = 0; i < str.length() / 2; i++) {
-    //         int n = str.length();
-    //         if (str.charAt(i) != str.charAt(n - 1 - i)) { // not a palindrome
-
-    //             return false;
-    //         }
-    //     }
-    //     return true; 
-    // }
-//-----------Q2:Route containing 4 directions (E,W,N,S) find a shortest path to reach the destination------------
-// public static float getShortestpath( String path){
-//     int x=0;
-//     int y=0;
-//     for(int i=0;i<path.length();i++){
-//         //South
-// if(path.charAt(i)=='S'){
-// y--;
-// }
-// //North
-// else if(path.charAt(i)=='N'){
-//     y++;
-// } 
-// //West
-// else if(path.charAt(i)=='W'){
-//     x--;
-// }
-// //East
-// else{
-//     x++;
-// }
-//     }
-//   int X2= x*x;
-//    int Y2=y*y;
-//     return (float)Math.sqrt(X2+Y2);
-
-// }
-// --------Q4:For a given String convert each first letter of each word to uppercase.----------
-// public static String toUpperCase(String str) {
-//     StringBuilder sb=new StringBuilder("");
-//     char ch =Character.toUpperCase(str.charAt(0));
-//     sb.append(ch);
-//     for(int i=1; i<str.length();i++){
-//         if(str.charAt(i)==' ' && i<str.length()-1){
-//             sb.append(str.charAt(i));
-//             i++;
-//             sb.append(Character.toUpperCase(str.charAt(i)));
-
-//         }
-//         else{
-//             sb.append(str.charAt(i));
-//         }
-//     }
-//     return sb.toString();
-// }
-// public static String compress(String str) {
-//     String newStr="";
-//     for(int i=1; i<str.length();i++){
-//         Integer count=1;
-//         while(i<str.length()-1 && str.charAt(i)== str.charAt(i+1)){
-//             count++;
-//             i++;
-//         }
-//         newStr+=str.charAt(i);
-//         if(count>1){
-//             newStr+=count.toString();
-//         }
-//     }
-// return newStr;
-// }
-// USING STRINGBUILDER--EFFICIENT
-public static String compress(String str) {
-    StringBuilder newStr = new StringBuilder("");
-    for (int i = 0; i < str.length(); i++) {
-        int count = 1;
-        while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
-            count++;
-            i++;
+    // // TO PRINT ALL LETTERS OF A STRING
+    public static void printLetters(String str) {
+        for (int i = 0; i < str.length(); i++) {
+            System.out.print(str.charAt(i) + " ");
         }
-        newStr.append(str.charAt(i));
-        if (count > 1) {
-            newStr.append(count);
-        }
+
+        System.out.println();
     }
-    return newStr.toString();
-}
+
+    // Q1:CHECK IF A STRING IS A PALINDROME
+    public static boolean isPalindrome(String str) {
+        for (int i = 0; i < str.length() / 2; i++) {
+            int n = str.length();
+            if (str.charAt(i) != str.charAt(n - 1 - i)) { // not a palindrome
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Q2:Route containing 4 directions (E,W,N,S) find a shortest path to reach the
+    // destination
+    public static float getShortestpath(String path) {
+        int x = 0;
+        int y = 0;
+        for (int i = 0; i < path.length(); i++) {
+            // South
+            if (path.charAt(i) == 'S') {
+                y--;
+            }
+            // North
+            else if (path.charAt(i) == 'N') {
+                y++;
+            }
+            // West
+            else if (path.charAt(i) == 'W') {
+                x--;
+            }
+            // East
+            else {
+                x++;
+            }
+        }
+        int X2 = x * x;
+        int Y2 = y * y;
+        return (float) Math.sqrt(X2 + Y2);
+
+    }
+
+    // To print subString of a String
+    public static String subString(String str1, int si, int ei) {
+        String subStr = " ";
+        for (int i = si; i < ei; i++) {
+            subStr += str1.charAt(i);
+        }
+        return subStr;
+    }
+
+    // Q4:For a given String convert each first letter of each word to
+    // uppercase.
+    public static String toUpperCase(String str) {
+        StringBuilder sb = new StringBuilder("");
+        char ch = Character.toUpperCase(str.charAt(0));
+        sb.append(ch);
+        for (int i = 1; i < str.length(); i++) {
+            if (str.charAt(i) == ' ' && i < str.length() - 1) {
+                sb.append(str.charAt(i));
+                i++;
+                sb.append(Character.toUpperCase(str.charAt(i)));
+
+            } else {
+                sb.append(str.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+
+    // Q5:String Compression
+    public static String compress(String str) {
+        String newStr = "";
+        for (int i = 1; i < str.length(); i++) {
+            Integer count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newStr += str.charAt(i);
+            if (count > 1) {
+                newStr += count.toString();
+            }
+        }
+        return newStr;
+    }
+
+    // //USING STRINGBUILDER--EFFICIENT
+    public static String compress(String str) {
+        StringBuilder newStr = new StringBuilder("");
+        for (int i = 0; i < str.length(); i++) {
+            int count = 1;
+            while (i < str.length() - 1 && str.charAt(i) == str.charAt(i + 1)) {
+                count++;
+                i++;
+            }
+            newStr.append(str.charAt(i));
+            if (count > 1) {
+                newStr.append(count);
+            }
+        }
+        return newStr.toString();
+    }
 
     public static void main(String[] args) {
-        // char arr[]={'a','b','c','d'};
-        // String str="abcd";
-        // String str2= new String("xyz@13245");
+        // STRING DECLARATION
+        char arr[] = { 'a', 'b', 'c', 'd' };
+        String str = "abcd";
+        String str2 = new String("xyz@13245");
 
-        // Scanner sc= new Scanner(System.in);
-        // String name;
+        Scanner sc = new Scanner(System.in);
+        String name;
 
-        // name= sc.nextLine();
-        // System.out.println(name);
+        name = sc.nextLine();
+        System.out.println(name);
+        // TO FIND THE LENGTH OF STRING
+        String fullName = "Nida Fatima";
+        System.out.println(fullName.length());
+        // Concatenation
+        String firstName = "Nida";
+        String lastName = "Fatima";
+        String fullName = firstName + " " + lastName;
+        System.out.println(fullName);
+        // TO PRINT ANY PARTICULAR LETTER OF A STRING
+        System.out.println(fullName.charAt(0));
+        System.out.println(fullName.charAt(1));
+        System.out.println(fullName.charAt(10));
+        // TO PRINT ALL LETTERS OF A STRING
+        printLetters(fullName);
+        // Q1:CHECK IF A STRING IS A PALINDROME
+        String str = "nida";
+        System.out.println(isPalindrome(str));
+        // Q2:Route containing 4 directions (E,W,N,S) find a shortest path to reach the
+        // destination
+        String path = "WNEENESENNN";
+        System.out.println(getShortestpath(path));
+        // COMPARISION OF TWO STRINGS
+        String S1 = "Nida";
+        String S2 = "Nida";
+        String S3 = new String("Nida");
+        if (S1 == S2) {
+            System.out.println("Strings are Equal");
+        } else {
+            System.out.println("Strings are not Equal");
+        }
+        // Use function (Correct way to check equality of two Strings using
+        // str1.equals(str2))
+        if (S1.equals(S3)) {
+            System.out.println("Strings are Equal");
+        } else {
+            System.out.println("Strings are not Equal");
+        }
+        // SUBSTRINGS
 
-        // String fullName="Nida Fatima";
-        // System.out.println(fullName.length());
-        // concatenation
-        // String firstName="Nida";
-        // String lastName="Fatima";
-        // String fullName= firstName+ " "+lastName;
-        // System.out.println(fullName);
-        // printLetters(fullName);
-        // -------------Q1:CHECK IF A STRING IS A PALINDROME---------
-        // String str = "noon ";
-        // System.out.println(isPalindrome(str));
-        //----Q2:Route containing 4 directions (E,W,N,S) find a shortest path to reach the destination--
-    //     String path="WNEENESENNN";
-    // System.out.println( getShortestpath(path));
-    //------COMPARISION OF TWO STRINGS------
-//     String S1="Nida";
-//     String S2="Nida";
-//     String S3= new String("Fatima");
-//     if(S1==S2){
-//         System.out.println("Strings are Equal");
-//     }else{
-//         System.out.println("Strings are not Equal");
-//     }
-//   if(S1.equals(S3))  {
-//     System.out.println("Strings are Equal");
-//   }else{
-//      System.out.println("Strings are not Equal");
-//   }
-//------SUBSTRINGS---------
-// String str="HELLO WORLD";
-// System.out.println(str.substring(0,5));
-//----------Q3:Set of Strings and we have to print the Largest String------
-// String fruits[]={"Apple","Mango","Banana"};
-// String largest= fruits[0];
-// for(int i=1;i<fruits.length;i++){
-//     if(largest.compareTo(fruits[i])<0){
-//         largest=fruits[i];
-//     }
-// }
-// System.out.println(largest);
-//----------STRING BUILDER-----
-// StringBuilder sb=new StringBuilder("");
-// for(char ch='a';ch<='z'; ch++){
-// sb.append(ch);
-// }
-// System.out.println(sb);
-// System.out.println(sb.length());
-// --------Q4:For a given String convert each first letter of each word to uppercase.----------
-// String str="nida fatima";
-// System.out.println(toUpperCase(str));
-// ----Q5:String Compression-----
-String str="aaabbcccdd";
-System.out.println(compress(str));
+        String str1 = "Hello Guys";
+        System.out.println(subString(str1, 5, 8));
+        // SUBSTRINGS
+        String str = "HELLO WORLD";
+        System.out.println(str.substring(0, 5));
+        // Q3:Set of Strings and we have to print the Largest String------
+        String fruits[] = { "Apple", "Mango", "Banana" };
+        String largest = fruits[0];
+        for (int i = 1; i < fruits.length; i++) {
+            if (largest.compareTo(fruits[i]) < 0) {
+                largest = fruits[i];
+            }
+        }
+        System.out.println(largest);
+        // STRING BUILDER
+        StringBuilder sb = new StringBuilder("");
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            sb.append(ch);
+        }
+        System.out.println(sb);
+        System.out.println(sb.length());
+        // Q4:For a given String convert each first letter of each word to uppercase.
+        String str = "hi i am nida fatima";
+        System.out.println(toUpperCase(str));
+        // Q5:String Compression
+        String str = "aaabbcccdd";
+        System.out.println(compress(str));
 
-    } 
-} 
+    }
+}
