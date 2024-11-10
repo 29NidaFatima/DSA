5d978d30cfa92bd4032800fba3e9fb9ed0f54a16
NidaFatima <2529nida17@gmail.com>
2024-11-10 15:07:10 +0530

    "Bit manipulation"

--- BitManipulation.java
+++ BitManipulation.java
@@ -1,5 +1,9 @@
 public class BitManipulation {
-    Q1-Check if a Number is Even or Odd
+    Q1-Check if
+    a Number
+    is Even
+    or Odd
+
     public static void oddOrEven(int n) {
         int bitMask = 1;
         if ((n & bitMask) == 0) {
@@ -36,57 +40,93 @@ public class BitManipulation {
     }
 
     // Update ith bit
- public static int updateIthBit(int n,int i,int newBit){
-    if (newBit==0) {
-        return clearIthBit(n, i);
-    }
-    else{
-        return setIthBit(n, i);
-    }
-     //  2nd approach to solve the same problem
- n=clearIthBit(0, 0);
-int BitMask= newBit<<i;
-return n| BitMask;
- }
-// Clear Last i bits
-public static int clearLastIbit( int n, int i){
-int bitmask=((-1)<<i);
-return n & bitmask;
-}
-//Clear Range of bits
-public static int clearBitsInRange(int n,int i,int j){
-int a=((~0)<<(j+1));
-int b=(1<<i)-1;
-int bitMask=a|b;
-return n & bitMask;
-}
-// check if a number is power of 2
-public static boolean isPowerofTwo(int n){
-    return (n&(n-1))==0;
-}
-// Count set bits in a number
-public static int countSetBits(int n){
-int count=0;
-while (n>0) {
-    if ((n&1)!=0) {
-        count++;
-    }
-    n=n>>1;
-}
-return count;
-}
-//Fast exponentiation
-public static int fastExpo(int a,int n){
-    int ans=1;
-    while (n>0) {
-        if ((n&1)!=0) {
-            ans=ans*a;
+    public static int updateIthBit(int n, int i, int newBit) {
+        if (newBit == 0) {
+            return clearIthBit(n, i);
+        } else {
+            return setIthBit(n, i);
         }
-        a=a*a;
-        n=n>>1;
+        // 2nd approach to solve the same problem
+        n = clearIthBit(0, 0);
+        int BitMask = newBit << i;
+        return n | BitMask;
     }
-    return ans;
-}
+
+    // Clear Last i bits
+    public static int clearLastIbit(int n, int i) {
+        int bitmask = ((-1) << i);
+        return n & bitmask;
+    }
+
+    // Clear Range of bits
+    public static int clearBitsInRange(int n, int i, int j) {
+        int a = ((~0) << (j + 1));
+        int b = (1 << i) - 1;
+        int bitMask = a | b;
+        return n & bitMask;
+    }
+
+    // check if a number is power of 2
+    public static boolean isPowerofTwo(int n) {
+        return (n & (n - 1)) == 0;
+    }
+
+    // Count set bits in a number
+    public static int countSetBits(int n) {
+        int count = 0;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                count++;
+            }
+            n = n >> 1;
+        }
+        return count;
+    }
+
+    // Fast exponentiation
+    public static int fastExpo(int a, int n) {
+        int ans = 1;
+        while (n > 0) {
+            if ((n & 1) != 0) {
+                ans = ans * a;
+            }
+            a = a * a;
+            n = n >> 1;
+        }
+        return ans;
+    }
+
+    // Modular exponentiation
+    public static int modularExponentiation(int a, int b, int m) {
+        int result = 1;
+        a = a % m; // Update a if it is more than or equal to m
+
+        while (b > 0) {
+            // If b is odd, multiply result by current a
+            if (b % 2 == 1) {
+                result = (result * a) % m;
+            }
+            // Square a and reduce b by half
+            a = (a * a) % m;
+            b /= 2;
+        }
+        return result;
+    }
+
+    // Different method
+    public static int modularExponentiation(int a, int b, int m) {
+        int result = 1;
+        while (b > 0) {
+            // If b is odd, multiply result by current a
+            if ((b & 1) == 1) {
+                result = (result % m * a % m) % m;
+            }
+            // Square a and reduce b by half
+            a = (a % m * a % m) % m;
+            b = b >> 1;
+        }
+        return result;
+    }
 
     public static void main(String[] args) {
         // Binary AND Bitwise OPERATOR
@@ -115,17 +155,23 @@ public static int fastExpo(int a,int n){
         // clear ith bit
         int result = clearIthBit(10, 1);
         System.out.println("Ith bit is:" + result);
-          // Update ith bit
-          updateIthBit(10, 2, 1);
-// Clear Last i bits
- int result=clearLastIbit(15, 2);
- System.out.println(result);
-//Clear Range of bits
-System.out.println(clearBitsInRange(10,2,4));
-// check if a number is power of 2
-System.out.println(isPowerofTwo(4));
-System.out.println(countSetBits(10));
-//Fast exponentiation
-System.out.println(fastExpo(3, 5));
+        // Update ith bit
+        updateIthBit(10, 2, 1);
+        // Clear Last i bits
+        int result = clearLastIbit(15, 2);
+        System.out.println(result);
+        // Clear Range of bits
+        System.out.println(clearBitsInRange(10, 2, 4));
+        // check if a number is power of 2
+        System.out.println(isPowerofTwo(4));
+        System.out.println(countSetBits(10));
+        // Fast exponentiation
+        System.out.println(fastExpo(3, 5));
+        // Modular exponentiation
+        int a = 3;
+        int b = 13;
+        int m = 7;
+        System.out.println("Result: " + modularExponentiation(a, b, m));
     }
+
 }
