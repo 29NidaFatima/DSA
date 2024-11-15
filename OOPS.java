fa247ad9a7bfb26671e768248fd72d256dacce46
NidaFatima <2529nida17@gmail.com>
2024-11-15 14:45:16 +0530

    oops

--- OOPS.java
+++ OOPS.java
@@ -1,47 +1,63 @@
 public class OOPS {
 
     public static void main(String[] args) {
-        // // OBJECT
-        Pen p1= new Pen(); //created a pen object called p1
+        // OBJECT
+        Pen p1 = new Pen(); // Created a Pen object called p1
         p1.setColor("blue");
-        System.out.println(p1.color);
+        System.out.println(p1.getColor());
         p1.setTip(5);
-        System.out.println(p1.tip);
-
+        System.out.println(p1.getTip()); // used the getter method to print the tip value
+        p1.setColor("Yellow"); // changed the color
+System.out.println(p1.getColor());
         //// Bank Account object
-        BankAccount myAcc = new BankAccount(); // here myAcc is a object
-myAcc.username="Nida Fatima";
-myAcc.setpassword("abc");
-    }
-}
-//  // CLASS
-class Pen{
-    // properties+function
-    String color;
-    int tip;
-    void setColor(String newColor){
-        color = newColor;
-     }
-     void setTip(int newTip){
-        tip=newTip;
-     }
-   
-}
-
-class Student{
-    String name;
-    int age;
-    float percentage;
-    void calculatePercentage( int phy,int chem,int math){
-percentage=phy+chem+math/3;
+        // BankAccount myAcc = new BankAccount(); // Created a BankAccount object called myAcc
+        // myAcc.username = "Nida Fatima";
+        // myAcc.setPassword("abc"); // called the `setPassword` method
     }
 }
 
-////ACCESS MODIFIERS
- class BankAccount{
-public String username;
-private String password;
-public void setpassword(String pwd){
-    password=pwd;
+// CLASS
+class Pen {
+    // Properties + functions
+    private String color;
+    private int tip;
+
+    // Getters
+    String getColor() {
+        return this.color;
+    }
+
+    int getTip() {
+        return this.tip;
+    }
+
+    // Setters
+    void setColor(String newColor) {
+        this.color = newColor;
+    }
+
+    void setTip(int newTip) {
+        this.tip = newTip;
+    }
 }
-}
+
+// // ----Student Class----
+// class Student {
+//     String name;
+//     int age;
+//     float percentage;
+
+//     void calculatePercentage(int phy, int chem, int math) {
+//         percentage = (phy + chem + math) / 3.0f; // Fixed: Corrected the formula to compute average correctly
+//     }
+// }
+
+// // ACCESS MODIFIERS
+// class BankAccount {
+//     public String username;
+//     private String password;
+
+//     public void setPassword(String pwd) { // Fixed: Method name should follow Java naming conventions
+//         this.password = pwd;
+//     }
+// }
