ab0ffd049c3a4387c5a473861fe342646620e658
NidaFatima <2529nida17@gmail.com>
2024-11-15 13:56:41 +0530

    "OOPS"

--- OOPS.java
+++ OOPS.java
@@ -1,12 +1,20 @@
 public class OOPS {
+
     public static void main(String[] args) {
+        // // OBJECT
         Pen p1= new Pen(); //created a pen object called p1
         p1.setColor("blue");
         System.out.println(p1.color);
         p1.setTip(5);
         System.out.println(p1.tip);
+
+        //// Bank Account object
+        BankAccount myAcc = new BankAccount(); // here myAcc is a object
+myAcc.username="Nida Fatima";
+myAcc.setpassword("abc");
     }
 }
+//  // CLASS
 class Pen{
     // properties+function
     String color;
@@ -19,6 +27,7 @@ class Pen{
      }
    
 }
+
 class Student{
     String name;
     int age;
@@ -26,4 +35,13 @@ class Student{
     void calculatePercentage( int phy,int chem,int math){
 percentage=phy+chem+math/3;
     }
+}
+
+////ACCESS MODIFIERS
+ class BankAccount{
+public String username;
+private String password;
+public void setpassword(String pwd){
+    password=pwd;
+}
 }
