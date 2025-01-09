0999352914fa588a0f977d93a79bda41c5b34a7d
NidaFatima <2529nida17@gmail.com>
2025-01-09 15:19:05 +0530

    abstraction

--- Abstraction.java
+++ Abstraction.java
@@ -1,58 +1,69 @@
 public class Abstraction {
     public static void main(String[] args) {
-        Horse h=new Horse();
+        Horse h = new Horse();
         h.eat();
         h.walk();
         System.out.println(h.color);
 
-        Chicken c=new Chicken();
+        Chicken c = new Chicken();
         h.walk();
-     
+
         c.eat();
         c.walk();
 
         // Hierarchy of constructor
-        Mustang myHorse=new Mustang();
-    //Animal-->Horse-->Mustang
+        Mustang myHorse = new Mustang();
+        // Animal-->Horse-->Mustang
     }
 }
-//abstract class using abstract keyword
+
+// abstract class using abstract keyword
 abstract class Animal {
     String color;
-    Animal(){
-    System.out.println("Animal constructor called");
+
+    Animal() {
+        System.out.println("Animal constructor called");
     }
-// non-abstract method(function)
-    void eat(){
+
+    // non-abstract method(function)
+    void eat() {
         System.out.println("Animal eats");
     }
+
     // abstract method(function)
-    abstract void walk(); //there is no implementation(defination not defined),basically it gives you an idea
+    abstract void walk(); // there is no implementation(defination not defined),basically it gives you an
+                          // idea
 }
-//sub-class
-class Horse extends Animal{
-    Horse(){
+
+// sub-class
+class Horse extends Animal {
+    Horse() {
         System.out.println("Horse constructor called");
     }
-    void changeColor(){
-        color="dark brown";
+
+    void changeColor() {
+        color = "dark brown";
     }
-    void walk(){
+
+    void walk() {
         System.out.println("walks on 4 legs");
     }
 }
- class Mustang  extends Horse{
-Mustang(){
-    System.out.println("Mustang constructor is called");
-}
-    
-}
-//sub-class
-class Chicken extends Animal{
-    void changeColor(){
-        color="yellow";
+
+class Mustang extends Horse {
+    Mustang() {
+        System.out.println("Mustang constructor is called");
     }
-    void walk(){
+
+}
+
+// sub-class
+class Chicken extends Animal {
+    void changeColor() {
+        color = "yellow";
+    }
+
+    void walk() {
         System.out.println("walks on 2 legs");
     }
 }
