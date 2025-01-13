9c6ef2c50781763c96692861aebd46eab0bfce9b
NidaFatima <2529nida17@gmail.com>
2025-01-13 13:44:57 +0530

    "recursion"

--- Recursion.java
+++ Recursion.java
@@ -1,5 +1,5 @@
 public class Recursion {
-            // Print numbers from n to 1(Decreasing Order)
+    // Print numbers from n to 1(Decreasing Order)
     public static void printDec(int n) {
         // Base Case
         if (n == 1) {
@@ -9,38 +9,37 @@ public class Recursion {
         System.out.print(n + " ");
         printDec(n - 1);
     }
-            // Print numbers from n to 1(Increasing Order)
+
+    // Print numbers from n to 1(Increasing Order)
     public static void printInc(int n) {
-         // Base Case
-         if (n == 1) {
-            System.out.print(n+" ");
+        // Base Case
+        if (n == 1) {
+            System.out.print(n + " ");
             return;
         }
-         printInc(n-1);
-         System.out.print(n + " ");
+        printInc(n - 1);
+        System.out.print(n + " ");
     }
-        // print factorial of a number
-        public static int fact(int n){
-            if (n==0) {
-                return 1;
-            }
-            int fnm1= fact(n-1);
-            int fn=n*fact(n-1);
-            return fn;
-        }
-    
-       
 
+    // print factorial of a number
+    public static int fact(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        int fnm1 = fact(n - 1);
+        int fn = n * fact(n - 1);
+        return fn;
+    }
 
     public static void main(String[] args) {
         // Print numbers from n to 1(Decreasing Order)
         int n = 10;
         printDec(n);
         // Print numbers from n to 1(Increasing Order)
-            int n = 10;
-            printInc(n);
-            // print factorial of a number
-            int n=5;
-         System.out.println(fact(n));
+        int n = 10;
+        printInc(n);
+        // print factorial of a number
+        int n = 5;
+        System.out.println(fact(n));
     }
 }
