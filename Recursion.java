8f53693f265f337c36aa5ad88891587246d64f02
NidaFatima <2529nida17@gmail.com>
2025-01-13 12:39:21 +0530

    recursion

--- Recursion.java
+++ Recursion.java
@@ -1,17 +1,34 @@
 public class Recursion {
-    public static void printDec(int n) {
-        // Base Case
-        if (n == 1) {
-            System.out.println(n);
+            // // Print numbers from n to 1(Decreasing Order)
+    // public static void printDec(int n) {
+    //     // Base Case
+    //     if (n == 1) {
+    //         System.out.println(n);
+    //         return;
+    //     }
+    //     System.out.print(n + " ");
+    //     printDec(n - 1);
+    // }
+    //         // Print numbers from n to 1(Increasing Order)
+    public static void printInc(int n) {
+         // Base Case
+         if (n == 1) {
+            System.out.print(n+" ");
             return;
         }
-        System.out.print(n + " ");
-        printDec(n - 1);
+         printInc(n-1);
+         System.out.print(n + " ");
     }
+      
+       
+
 
     public static void main(String[] args) {
-        // Print numbers from n to 1(Decreasing Order)
-        int n = 10;
-        printDec(n);
+        // // Print numbers from n to 1(Decreasing Order)
+        // int n = 10;
+        // printDec(n);
+        // Print numbers from n to 1(Increasing Order)
+            int n = 10;
+            printInc(n);
     }
 }
