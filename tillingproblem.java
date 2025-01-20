f810db53559e717f50da06705e71c00cf48e5f01
NidaFatima <2529nida17@gmail.com>
2025-01-20 11:30:28 +0530

    "tillingProblem"

--- tillingproblem.java
+++ tillingproblem.java
@@ -5,10 +5,12 @@ public class tillingproblem {
         if (n == 0 || n == 1) {
             return 1;
         }
-        int fnm1 = tillingProblem(n - 1);
-        int fnm2 = tillingProblem(n - 2);
-        int totalWays=fnm1 + fnm2;
-        return  totalWays;
+        // int fnm1 = tillingProblem(n - 1);
+        // int fnm2 = tillingProblem(n - 2);
+        // int totalWays=fnm1 + fnm2;
+        // return  totalWays;
+                // // Another way to write above code in less no.of line
+                return tillingProblem(n - 1)+tillingProblem(n - 2);
 
     }
 
