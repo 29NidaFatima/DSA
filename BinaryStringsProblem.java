3edf0fc5f6afbb7ba6e824b628e86f49cc15a789
NidaFatima <2529nida17@gmail.com>
2025-01-21 10:48:53 +0530

    .

--- BinaryStringsProblem.java
+++ BinaryStringsProblem.java
@@ -1,24 +1,22 @@
 public class BinaryStringsProblem {
-    public static void printBinStrings(int n,int lastPlace,String str){
-// if (lastPlace==0) {
-//     printBinStrings(n-1, 0, str.append("0"));
-//     printBinStrings(n-1, 1,str.append("1"));
-// }
-// else{
-//     printBinStrings(n-1, 0, str.append("0"));
-// }
-//base case
-if (n==0) {
-    System.out.println(str);
-    return;
-}
-printBinStrings(n-1, 0, str+"0");
+    public static void printBinStrings(int n, int lastPlace, String str) {
+        // Base case
+        if (n == 0) {
+            System.out.println(str);
+            return;
+        }
 
-if (lastPlace==0) {
-    printBinStrings(n-1, 1,str+"1");
-}
+        // Add '0' to the string and recursively call
+        printBinStrings(n - 1, 0, str + "0");
+
+        // If the last place was 0, add '1' to the string and recursively call
+        if (lastPlace == 0) {
+            printBinStrings(n - 1, 1, str + "1");
+        }
     }
+
     public static void main(String[] args) {
-        System.out.println(printBinStrings(3, 0, ""));
+ 
+        printBinStrings(3, 0, "");
     }
 }
