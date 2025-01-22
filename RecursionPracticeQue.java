64b0276a76a52369c12084262e3a936636774be8
NidaFatima <2529nida17@gmail.com>
2025-01-22 15:52:58 +0530

    "recursion"

--- RecursionPracticeQue.java
+++ RecursionPracticeQue.java
@@ -1,14 +1,16 @@
 public class RecursionPracticeQue {
-    // public static void indicesOfNum(int arr[], int key, int i) {
-    //     //Base Case
-    //     if (i == arr.length) {
-    //         return;
-    //     }
-    //     if (arr[i] == key) {
-    //         System.out.print(i + " ");
-    //     }
-    //     indicesOfNum(arr, key, i + 1);
-    // }
+    // Q1
+    public static void indicesOfNum(int arr[], int key, int i) {
+        //Base Case
+        if (i == arr.length) {
+            return;
+        }
+        if (arr[i] == key) {
+            System.out.print(i + " ");
+        }
+        indicesOfNum(arr, key, i + 1);
+    }
+    // Q2
     static String[] words = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 public static void printDigits(int number){
     if (number==0) {
@@ -19,10 +21,12 @@ public static void printDigits(int number){
     System.out.println(words[lastDigit]+" ");
 }
     public static void main(String[] args) {
-        // int arr[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
-        // int key = 2;
-        // indicesOfNum(arr, key, 0);
-        printDigits(1234);
+        // Q1
+        int arr[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
+        int key = 2;
+        indicesOfNum(arr, key, 0);
+        // Q2
+        printDigits(4567);
 
     }
 }
