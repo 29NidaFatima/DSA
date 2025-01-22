af1bc9acdcf428443404024ea4b39a819fa6151e
NidaFatima <2529nida17@gmail.com>
2025-01-22 12:43:15 +0530

    1

--- RecursionPracticeQue.java
+++ RecursionPracticeQue.java
@@ -9,6 +9,7 @@ public class RecursionPracticeQue {
         }
         indicesOfNum(arr, key, i + 1);
     }
+    
 
     public static void main(String[] args) {
         int arr[] = { 3, 2, 4, 5, 6, 2, 7, 2, 2 };
