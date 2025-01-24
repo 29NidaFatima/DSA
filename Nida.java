23d6783eee9710b6a844054f1c45d9d5330857d1
NidaFatima <2529nida17@gmail.com>
2025-01-24 17:31:45 +0530

    .

--- Nida.java
+++ Nida.java
@@ -1,5 +1,8 @@
 public class Nida {
     public static void main(String[] args) {
         System.out.println("Nidaaaaa");
+        int a=10;
+        int b=10;
+        System.out.println(a+b);
     }
 }
