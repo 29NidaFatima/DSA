e49979d6bd56930f97e9202ba8491bd79371d18b
NidaFatima <2529nida17@gmail.com>
2025-02-03 10:59:06 +0530

    search

--- Divide&Conquer/searchInRotatedArray.java
+++ Divide&Conquer/searchInRotatedArray.java
@@ -38,7 +38,7 @@ public class searchInRotatedArray {
     public static void main(String[] args) {
         int arr[] = {4, 5, 6, 7, 0, 1, 2};
         int target = 0; // Expected output: 4
-        int taridx = search(arr, target, 0, arr.length - 1);
-        System.out.println(taridx);
+        int tarIdx = search(arr, target, 0, arr.length - 1);
+        System.out.println(tarIdx);
     }
 }
