aa298a2503ba63f369e69f20bf10e6aee64bc26c
NidaFatima <2529nida17@gmail.com>
2025-02-17 12:58:38 +0530

    FindSubsets

--- BackTracking/FindSubsets.java
+++ BackTracking/FindSubsets.java
@@ -1,13 +1,18 @@
 public class FindSubsets {
     public static void findSubsets(String str, String ans, int i) {
-        // Base case: When we reach the end of the string, print the subset
-   if (ans.length()==0) {
-    System.out.println("null");
-   } else {
-    System.out.println(ans);
-   }
+        // Base case: When we reach the end of the string
+        if (i == str.length()) {
+            if (ans.length() == 0) {
+                System.out.println("null"); // If the subset is empty, print "null"
+            } else {
+                System.out.println(ans); // Otherwise, print the subset
+            }
+            return; // Return to stop further recursion
+        }
+
         // Choice 1: Include the current character
         findSubsets(str, ans + str.charAt(i), i + 1);
+
         // Choice 2: Exclude the current character
         findSubsets(str, ans, i + 1);
     }
