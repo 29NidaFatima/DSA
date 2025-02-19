146cee4fa7aef388b8dde8b0754c4a311f32eba9
NidaFatima <2529nida17@gmail.com>
2025-02-19 11:23:25 +0530

    findpermutations

--- BackTracking/FindPermutations.java
+++ BackTracking/FindPermutations.java
@@ -1,19 +1,22 @@
+
 public class FindPermutations {
-    public static void findPermutation(String str,String ans){
+
+    public static void findPermutation(String str, String ans) {
 //BASE CASE
-if (str.length()==0) {
-    System.out.println(ans);
-    return;
-}
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
 // recursion
-for (int i = 0; i < str.length(); i++) {
-     char curr=str.charAt(i);
-   String  Newstr=str.substring(0,i)+str.substring(i+1);
-     findPermutation(Newstr, ans+curr);
-}
+        for (int i = 0; i < str.length(); i++) {
+            char curr = str.charAt(i);
+            String Newstr = str.substring(0, i) + str.substring(i + 1);
+            findPermutation(Newstr, ans + curr);
+        }
     }
+
     public static void main(String[] args) {
-        String str="abc";
+        String str = "abc";
         findPermutation(str, "");
     }
 }
