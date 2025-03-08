c312530b4a126ecd224740ea715fb79816a433a3
NidaFatima <2529nida17@gmail.com>
2025-03-08 11:18:33 +0530

    "CheckLeapYear"

--- TCS NQT/CheckLeapYear.java
+++ TCS NQT/CheckLeapYear.java
@@ -2,15 +2,15 @@
 import java.util.Scanner;
 
 public class CheckLeapYear {
+
     public static void main(String[] args) {
         int year;
         System.out.println("Enter your year");
-        Scanner sc= new Scanner(System.in);
-        year=sc.nextInt();
-        if(year%100==0 && year%400==0 || year%100!=0 && year%4==0){
+        Scanner sc = new Scanner(System.in);
+        year = sc.nextInt();
+        if (year % 100 == 0 && year % 400 == 0 || year % 100 != 0 && year % 4 == 0) {
             System.out.println("Leap year");
-        }
-        else{
+        } else {
             System.out.println("Not a leap year");
         }
     }
