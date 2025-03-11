00d412e004df2d3a47fa92bd288fbae85c1c46b3
NidaFatima <2529nida17@gmail.com>
2025-03-11 15:16:10 +0530

    "SumOfArrayElements"

--- TCS NQT/ArrayBasedProblem/SumOfArrayElements.java
+++ TCS NQT/ArrayBasedProblem/SumOfArrayElements.java
@@ -16,3 +16,22 @@ public class SumOfArrayElements {
         System.out.println("The sum of the array elements is: " + result); 
     }
 }
+//-------------Another way to write same code ------
+public class SumOfArrayElements {
+    public static int sum(int arr[]) {
+        if (arr == null) {
+            return 0; // Handle null input
+        }
+        int sum = 0;
+        for (int num : arr) {
+            sum += num; // Use enhanced for loop and += operator
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {1, 5, 6, 7, 8, 3, 4};
+        int result = sum(arr);
+        System.out.println("The sum of the array elements is: " + result);
+    }
+}
