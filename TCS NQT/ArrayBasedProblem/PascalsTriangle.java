4ad8a35a83577778f6a367094052bcafc79bdb50
NidaFatima <2529nida17@gmail.com>
2025-03-13 13:08:23 +0530

    "PascalsTriangle"

--- TCS NQT/ArrayBasedProblem/PascalsTriangle.java
+++ TCS NQT/ArrayBasedProblem/PascalsTriangle.java
@@ -1,6 +1,6 @@
-public class PascalsTriangle {
 
-    public  static void pascalTriangle(int n){
+public class PascalsTriangle {
+    public static void pascalTriangle(int n) {
         for (int row = 1; row <= n; row++) {
             int number = 1; // First element of each row is always 1
             // Add spacing for alignment
@@ -13,13 +13,11 @@ public class PascalsTriangle {
                 number = number * (row - col) / col; // Calculate the next number
             }
             System.out.println();
-   
-    }
+        }
     }
     public static void main(String[] args) {
-       
-            int n = 5; // Number of rows
-            pascalTriangle(n);
-           
+        int n = 5; // Number of rows
+        pascalTriangle(n);
+
     }
 }
