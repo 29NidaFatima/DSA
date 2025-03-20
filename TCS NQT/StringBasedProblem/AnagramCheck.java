8036ac09a0e5c2af60daa0d75a3c27ace52a4cf4
NidaFatima <2529nida17@gmail.com>
2025-03-20 13:07:38 +0530

    "anagramCheck"

---
+++ TCS NQT/StringBasedProblem/AnagramCheck.java
@@ -0,0 +1,25 @@
+public class AnagramCheck {
+    public static boolean isAnagram(String str1, String str2) {
+        if (str1.length() != str2.length()) return false;
+
+        int[] charCount = new int[26]; // Assuming only lowercase English letters
+
+        for (int i = 0; i < str1.length(); i++) {
+            charCount[str1.charAt(i) - 'a']++;  // Increment count for str1
+            charCount[str2.charAt(i) - 'a']--;  // Decrement count for str2
+        }
+
+        for (int count : charCount) {
+            if (count != 0) return false;
+        }
+
+        return true;
+    }
+  
+    public static void main(String[] args) {
+        String str1 = "listen";
+        String str2 = "silent";
+      System.out.println(  isAnagram(str1, str2));
+
+    }
+}
