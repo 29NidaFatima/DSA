e98937e0bdfcd2e3bb809a7c0c5a63bdd838e653
NidaFatima <2529nida17@gmail.com>
2025-05-26 15:26:57 +0530

    pairSumOne

--- ArrayList/pairSumOne.java
+++ ArrayList/pairSumOne.java
@@ -1,17 +1,34 @@
 import java.util.ArrayList;
 
 public class pairSumOne {
-    // Brute force method to check if any two elements sum to target
-    public static boolean pairSumOne(ArrayList<Integer> list, int target) {
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                if (list.get(i) + list.get(j) == target) {
-                    return true;
-                }
-            }
-        }
-        return false; 
+    // // Brute force method to check if any two elements sum to target
+    // public static boolean pairSumOne(ArrayList<Integer> list, int target) {
+    //     for (int i = 0; i < list.size(); i++) {
+    //         for (int j = i + 1; j < list.size(); j++) {
+    //             if (list.get(i) + list.get(j) == target) {
+    //                 return true;
+    //             }
+    //         }
+    //     }
+    //     return false; 
+    // } 
+    // //optimal approach
+       public static boolean pairSumOne(ArrayList<Integer> list, int target) {
+int s=0;
+int e=list.size();
+while (s<e) {
+    if (list.get(s)+list.get(e)==target) {
+        return true;
     }
+    else if(list.get(s)+list.get(e)<target){
+        s++;
+    }
+    else{
+        e--;
+    }
+}
+return false;
+       }
 
     public static void main(String[] args) {
         ArrayList<Integer> list = new ArrayList<>();
