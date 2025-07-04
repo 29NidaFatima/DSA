d9313244d3e864981431565bccd38b08af59680a
NidaFatima <2529nida17@gmail.com>
2025-07-04 10:05:04 +0530

    LinkedList

--- LinkedList/LinkedList.java
+++ LinkedList/LinkedList.java
@@ -53,14 +53,14 @@ public class LinkedList {
         System.out.println("null");
     }
 
-    public void addMiddle(int idx, int data) { 
+    public void addMiddle(int idx, int data) {
         if (idx == 0) {
             addFirst(data);
             return;
         }
-      
+
         Node newNode = new Node(data);
-          size++;
+        size++;
         Node temp = head;
         int i = 0;
         while (i < idx - 1) {
@@ -185,82 +185,149 @@ public class LinkedList {
         // Step 5: Delete the node
         prev.next = prev.next.next;
     }
-  // Step 1
-public Node findMid(Node head) {
+    // Step 1
+
+    public Node findMid(Node head) {
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow; // slow is the middle node
+    }
+
+    public boolean checkPalindrome() {
+        // BASE CASE
+        if (head == null || head.next == null) {
+            return true;
+        }
+        // step1- find mid
+        Node midNode = findMid(head);
+        //step2- reverse 2nd half
+        Node prev = null;
+        Node curr = midNode;
+
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        Node right = prev;
+        Node left = head;
+        //step3-  check left half and right half
+        while (right != null) {
+            if (left.data != right.data) {
+                return false;
+            }
+            left = left.next;
+            right = right.next;
+        }
+        return true;
+    }
+// how can we detect loop in linkedList
+
+    public static boolean isCycle() {
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next; //+1 moves
+            fast = fast.next.next; //+2 moves
+            if (fast == slow) {
+                return true; //cycle exists
+            }
+
+        }
+        return false; //cycle does not exists
+    }
+
+public static void removeCycle(Node head) {
+    // Step 1: Detect cycle using Floyd’s Algorithm
     Node slow = head;
     Node fast = head;
-
+    boolean cycle = false;
 
     while (fast != null && fast.next != null) {
         slow = slow.next;
         fast = fast.next.next;
-    }
 
-    return slow; // slow is the middle node
-}
-public boolean  checkPalindrome(){
-    // BASE CASE
-    if(head==null || head.next==null){
-return true;
-    }
-    // step1- find mid
-    Node midNode=findMid(head);
-    //step2- reverse 2nd half
-    Node prev=null;
-    Node curr=midNode;
-   
-    Node next;
-    while (curr!=null) {
-       next=curr.next;
-       curr.next=prev;
-       prev=curr;
-       curr=next; 
-    }
-    Node right=prev;
-    Node left=head;
-    //step3-  check left half and right half
-    while (right!=null) {
-        if (left.data!=right.data) {
-            return false;
+        if (fast == slow) {
+            cycle = true;
+            break;
         }
-        left=left.next;
-        right=right.next;
     }
-    return true;
+
+    // Step 2: If no cycle found, return
+    if (cycle=false) {
+        return;
+    }
+
+    // Step 3: Find start of the loop
+    slow = head;
+    Node prev = null; // This will store the node just before meeting point
+
+    while (slow != fast) {
+        prev = fast;
+        slow = slow.next;
+        fast = fast.next;
+    }
+
+    // Step 4: Remove loop
+    prev.next = null;
 }
 
+
+    
 
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
+        //     LinkedList ll = new LinkedList();
 
-        ll.addFirst(2);
-        ll.addFirst(1);
-        ll.addLast(4);
-        ll.addLast(5);
-        ll.addMiddle(2, 3);
+        //     ll.addFirst(2);
+        //     ll.addFirst(1);
+        //     ll.addLast(4);
+        //     ll.addLast(5);
+        //     ll.addMiddle(2, 3);
+        //     ll.print();
+        //     ll.removeFirst();
+        //     ll.print();
+        //     ll.removeLast();
+        //     ll.print();
+        //     System.out.println("Size: " + ll.size);
+        //     System.out.println("Index of 3 : " + ll.itrSearch(3));
+        //     System.out.println("Index of 10: " + ll.itrSearch(10));
+        //     System.out.println("Index of 3 (recursive): " + ll.recSearch(3));
+        //     System.out.println("Index of 10 (recursive): " + ll.recSearch(10));
+        //     ll.reverse();
+        //     ll.print();
+        //     ll.deleteNthfromEnd(3);
+        //     ll.print();
+        //     ll.addFirst(1);
+        //     ll.addFirst(2);
+        //     ll.addLast(2);
+        //     ll.addLast(1);
+        //      ll.print(); //1->2->2->1->null
+        //    System.out.println(ll.checkPalindrome());
+        // PART 2
+        // --------------------
+        // head= new Node(1);
+        // head.next=new Node(2);
+        //   head.next.next=new Node(3);
+        //     head.next.next.next=head;
+        //     System.out.println(isCycle());
+        // --------------------
+        head = new Node(1);
+        Node temp = new Node(2);
+        head.next = temp;
+        head.next.next = new Node(3);
+        head.next.next.next = temp;
+        System.out.println(isCycle());
+        removeCycle(head);
+        System.out.println(isCycle());
 
-        ll.print();
-
-        ll.removeFirst();
-        ll.print();
-        ll.removeLast();
-        ll.print();
-
-        System.out.println("Size: " + ll.size);
-        System.out.println("Index of 3 : " + ll.itrSearch(3));
-        System.out.println("Index of 10: " + ll.itrSearch(10));
-        System.out.println("Index of 3 (recursive): " + ll.recSearch(3));
-        System.out.println("Index of 10 (recursive): " + ll.recSearch(10));
-
-        ll.reverse();
-        ll.print();
-        ll.deleteNthfromEnd(3);
-        ll.print();
-        ll.addFirst(1);
-        ll.addFirst(2);
-        ll.addLast(2);
-        ll.addLast(1);
-         ll.print(); //1->2->2->1->null
-       System.out.println(ll.checkPalindrome());
     }
 }
+
