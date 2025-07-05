dd8260b407ee5b4bef4e4a269b24a47eda9f7829
NidaFatima <2529nida17@gmail.com>
2025-07-05 22:43:30 +0530

    LinkedList

---
+++ LinkedList/mergeSort.java
@@ -0,0 +1,95 @@
+public class mergeSort {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    // Add node at the beginning
+    public static void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Print the linked list
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    private static Node getMid(Node head) {
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow; // mid node
+    }
+
+    private static Node merge(Node head1, Node head2) {
+        Node merged = new Node(-1);
+        Node temp = merged;
+
+        while (head1 != null && head2 != null) {
+            if (head1.data <= head2.data) {
+                temp.next = head1;
+                head1 = head1.next;
+            } else {
+                temp.next = head2;
+                head2 = head2.next;
+            }
+            temp = temp.next;
+        }
+
+        // Attach remaining elements
+        if (head1 != null) temp.next = head1;
+        if (head2 != null) temp.next = head2;
+
+        return merged.next;
+    }
+
+    public static Node mergeSort(Node head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+
+        Node mid = getMid(head);
+        Node rightHead = mid.next;
+        mid.next = null;
+
+        Node left = mergeSort(head);
+        Node right = mergeSort(rightHead);
+
+        return merge(left, right);
+    }
+
+    public static void main(String[] args) {
+
+        addFirst(5);
+        addFirst(4);
+        addFirst(7);
+        addFirst(2);
+        addFirst(6);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        head = mergeSort(head);
+
+        System.out.println("Sorted List:");
+        printList(head);
+    }
+}
