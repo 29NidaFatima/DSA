d8c1ee884dc21dc9739528c3b0758c8bd8ce4242
NidaFatima <2529nida17@gmail.com>
2025-07-07 11:30:22 +0530

    zigZag

---
+++ LinkedList/Zigzag.java
@@ -0,0 +1,86 @@
+public class Zigzag {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node head;
+
+    // Add node at the beginning
+    public static void addFirst(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    // Print the linked list
+    public static void printList(Node head) {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    // Zigzag the linked list
+    public void zigZag() {
+        // Step 1: Find mid
+        Node slow = head;
+        Node fast = head.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        Node mid = slow;
+
+        // Step 2: Reverse second half
+        Node curr = mid.next;
+        mid.next = null;
+        Node prev = null;
+        Node next;
+        while (curr != null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+        // Step 3: Alternate merge
+        Node left = head;
+        Node right = prev;
+        Node nextL, nextR;
+        while (left != null && right != null) {
+            nextL = left.next;
+            left.next = right;
+            nextR = right.next;
+            right.next = nextL;
+
+            left = nextL;
+            right = nextR;
+        }
+    }
+
+    public static void main(String[] args) {
+        Zigzag ll = new Zigzag();
+        addFirst(5);
+        addFirst(4);
+        addFirst(3);
+        addFirst(2);
+        addFirst(1);
+
+        System.out.println("Original List:");
+        printList(head);
+
+        ll.zigZag();
+
+        System.out.println("ZigZag List:");
+        printList(head);
+    }
+}
